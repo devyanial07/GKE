3658d0fd3cb54d79ff576e61b6c82ceec2ab989e
devyanial07 <34202154+devyanial07@users.noreply.github.com>
2023-02-18 22:59:33 +0000

    test dns

--- apis.tf
+++ apis.tf
@@ -7,3 +7,12 @@ resource "google_project_service" "api_iap" {
   project = var.project
   service = "iap.googleapis.com"
 }
+
+module "project_dns_api" {
+  source = "terraform-google-modules/project-factory/google//modules/project_services"
+  project_id                  = var.project
+  disable_services_on_destroy = false
+  activate_apis = [
+    "dns.googleapis.com"
+  ]
+}
