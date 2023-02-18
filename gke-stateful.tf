178918fb75590f4e0c1db611a892f329c0f233ea
devyanial07 <34202154+devyanial07@users.noreply.github.com>
2023-02-18 23:58:19 +0000

    gke test

--- gke-stateful.tf
+++ gke-stateful.tf
@@ -5,13 +5,13 @@ module "gke_stateful" {
     ]
     source = "./gke-stateful"
 
-    project_id                 = var.project
-    name                       = var.cluster_name
-    region                     = var.region
-    zones                      = var.cluster_zones
-    network                    = var.vpc_network_name
+    #project                    = var.project
+    #cluster_name               = var.cluster_name
+    #region                     = var.region
+    #cluster_zones              = var.cluster_zones
+    #network                    = var.vpc_network_name
     #subnetwork                 = "${var.vpc_network_name}-primary"
     #ip_range_pods              = "${var.vpc_network_name}-secondary-1"
     #ip_range_services          = "${var.vpc_network_name}-secondary-2"
-    kubernetes_version         = var.kubernetes_version
+    #kubernetes_version         = var.kubernetes_version
 }
