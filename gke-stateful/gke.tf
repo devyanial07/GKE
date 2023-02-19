130b73ca62e4226d772215320db6dbfad2eb3b36
devyanial07 <34202154+devyanial07@users.noreply.github.com>
2023-02-19 00:37:36 +0000

    gke

--- gke-stateful/gke.tf
+++ gke-stateful/gke.tf
@@ -116,3 +116,55 @@ module "gke" {
   logging_service    = "logging.googleapis.com/kubernetes"
 }
  */
+/* 
+module "gke" {
+  depends_on = [
+    google_project_iam_member.google_apis_service_agent
+    ]
+
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
+   version = "21.1.0"
+  
+  project_id                      = var.project_id
+  name                            = var.cluster_name
+  region                          = var.location
+  zones                           = var.cluster_zones
+  network                         = var.cluster_network
+  subnetwork                      = var.cluster_subnetwork
+  ip_range_pods                   = var.cluster_ip_range_pods
+  ip_range_services               = var.cluster_ip_range_svc
+  release_channel                 = var.release_channel
+  cluster_resource_labels         = var.cluster_resource_labels
+  identity_namespace              = var.identity_namespace
+  network_policy                  = var.network_policy
+  service_account                 = google_service_account.gke_sa.email
+  http_load_balancing             = true
+  horizontal_pod_autoscaling      = true
+  enable_private_endpoint         = var.enable_private_endpoint
+  enable_private_nodes            = true
+  enable_binary_authorization     = true
+  issue_client_certificate        = false
+  remove_default_node_pool        = true
+  kubernetes_version              = var.kubernetes_version
+  enable_vertical_pod_autoscaling = true
+  grant_registry_access           = true
+  master_authorized_networks      = var.master_authorized_networks
+  upstream_nameservers = []
+
+  node_metadata = "GKE_METADATA_SERVER"
+
+  add_master_webhook_firewall_rules = true
+  add_cluster_firewall_rules        = true
+  add_shadow_firewall_rules         = true
+
+
+  master_ipv4_cidr_block = var.master_ipv4_cidr_block
+
+  node_pools              = var.node_pools
+  node_pools_oauth_scopes = var.node_pools_oauth_scopes
+  
+
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+  logging_service    = "logging.googleapis.com/kubernetes"
+}
+ */
