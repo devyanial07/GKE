9e1a1858edffd4c8b6fdd2b17664530e673d317e
devyanial07 <34202154+devyanial07@users.noreply.github.com>
2023-02-19 03:06:58 +0000

    dns

--- domains.tf
+++ domains.tf
@@ -16,8 +16,8 @@ data "google_dns_managed_zone" "sq_zone" {
 
 
 resource "google_dns_record_set" "sq" {
-  name         = "sonarqube.${data.google_dns_managed_zone.sq_zone.dns_name}"
-  managed_zone = data.google_dns_managed_zone.sq_zone.dns_name
+  name         = "sonarqube.d-e-l-o.com"
+  managed_zone = "d-e-l-o.com"
   type         = "A"
   ttl          = 300
 
