4b57cc51afd2e6b2b7db2c69d0acda96322bb06c
devyanial07 <34202154+devyanial07@users.noreply.github.com>
2023-02-19 00:05:56 +0000

    gke st

--- gke-stateful.tf
+++ gke-stateful.tf
@@ -2,5 +2,14 @@ module "gke_stateful" {
   depends_on  = [
         module.vpc_network
     ]
-  source                     = "./gke-stateful"
+  source                 = "./gke-stateful"
+  project                = var.project
+  region                 = var.region
+  cluster_zones          = var.cluster_zones
+  cluster_name           = var.cluster_name
+  vpc_network            = var.vpc_network_name
+  vpc_subnet             = "${var.vpc_network_name}-primary"
+  vpc_subnet_pod         = "${var.vpc_network_name}-secondary-1"
+  vpc_subnet_svc         = "${var.vpc_network_name}-secondary-2"
+  kubernetes_version     = var.kubernetes_version
 }
