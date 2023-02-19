080d20c427b60ff52170f57ab7f2a0999ed461cf
devyanial07 <34202154+devyanial07@users.noreply.github.com>
2023-02-19 16:37:47 +0000

    new

--- gke-stateful.tf
+++ gke-stateful.tf
@@ -4,7 +4,7 @@ resource "google_service_account" "gke_sa" {
 }
 resource "google_project_iam_member" "gar_read" {
   project = var.project
-  role    = "roles/artifactregistry.reader"
+  role    = "roles/artifactregistry.admin"
   member  = "serviceAccount:${google_service_account.gke_sa.email}"
 }
 module "gke" {
