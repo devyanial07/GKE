132cf950a8afed15f438473af354fc540483f25e
devyanial07 <34202154+devyanial07@users.noreply.github.com>
2023-02-24 00:22:15 +0000

    cleanup

--- domains.tf
+++ domains.tf
@@ -8,25 +8,6 @@ resource "google_compute_global_address" "web_address" {
   project   =   var.project
 }
 
-data "google_dns_managed_zone" "sq_name_zone" {
-  name = "d-e-l-o-com."
-  project   =   var.project
-}
-
-output "zone_sonarqube" {
-    description = "sonarqube zone"
-    value = data.google_dns_managed_zone.sq_name_zone.name
-}
-
-/* resource "google_dns_record_set" "sq" {
-  name         = "sonarqube.${data.google_dns_managed_zone.sq_name_zone.name}"
-  managed_zone = data.google_dns_managed_zone.sq_name_zone.name
-  type         = "A"
-  ttl          = 300
-
-  rrdatas = ["34.105.224.15"]
-} */
-
 resource "google_dns_record_set" "webapp" {
   name         = "webapp.d-e-l-o.com."
   managed_zone = "d-e-l-o-com"
