ea0af696b79ae9480ed855c67c2a6cb89febd9ad
devyanial07 <34202154+devyanial07@users.noreply.github.com>
2023-07-23 21:32:22 +0100

    armor

---
+++ armor.tf
@@ -0,0 +1,19 @@
+resource "google_compute_security_policy" "policy" {
+     name = "wp-champ-policy"
+     description = "Cloud Armor Security Policy"
+     adaptive_protection_config {
+        layer_7_ddos_defense_config {
+          enable = true
+    }
+  }
+
+  deny = {
+      action      = "deny(403)"
+      priority    = 2000
+      description = "Deny pre-configured rule java-v33-stable at sensitivity level 3"
+      preview     = true
+      expression  = <<-EOT
+        evaluatePreconfiguredWaf('java-v33-stable', {'sensitivity': 3})
+      EOT
+    }
+}
